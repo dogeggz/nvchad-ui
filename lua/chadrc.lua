20b747efc3e2ea8aaf2843ec341070083177ac52
dogeggz <sleepb411@gmail.com>
2024-10-20 16:57:51 -0400

    refactor(config): move nvconfig, chadrc to here instead of config dir

---
+++ lua/chadrc.lua
@@ -0,0 +1,10 @@
+local M = {}
+M.base46 = {
+  theme_toggle = { "solarized_dark", "solarized_osaka" },
+  theme = "solarized_osaka", -- default theme
+  transparency = true, -- statusline, tabufline not transparent
+  changed_themes = {},
+  hl_add = {},
+  hl_override = {},
+}
+return M
--- lua/nvconfig.lua
+++ lua/nvconfig.lua
@@ -1,88 +1,110 @@
+local pallete = {
+  dark_gray = "#22383f",
+  light_gray = "#528694",
+  dark_blue = "#042034",
+  blue = "#1a6397",
+  light_blue = "#016ab8",
+  dark_red = "#57040e",
+  red = "#b7221e",
+  light_red = "#bc2e2c",
+  violet = "#494fb6",
+  black = "#151c28",
+  light_black = "#042034",
+  dark_green = "#2b3300",
+  light_green = "#bafb00",
+  magenta = "#f255a1",
+  white = "#fdf6e3",
+  pure_black = "#000000",
+}
+
 ---@type ChadrcConfig
 local options = {
-
   ui = {
-    -- hl = highlights
-    hl_add = {},
-    hl_override = {},
-    changed_themes = {},
-    theme_toggle = { "onedark", "one_light" },
-    theme = "onedark", -- default theme
-    transparency = false,
-
     cmp = {
       icons = true,
       lspkind_text = true,
       style = "default", -- default/flat_light/flat_dark/atom/atom_colored
     },
 
-    telescope = { style = "borderless" }, -- borderless / bordered
+    telescope = { style = "bordered" }, -- borderless / bordered
 
     statusline = {
+      enabled = true,
       theme = "default", -- default/vscode/vscode_colored/minimal
-      -- default/round/block/arrow separators work only for default statusline theme
-      -- round and block will work for minimal theme only
-      separator_style = "default",
-      order = nil,
+      separator_style = "round", -- deafult/round/block/arrow
+      order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
       modules = nil,
+      lualine = true,
     },
 
     -- lazyload it when there are 1+ buffers
     tabufline = {
       enabled = true,
       lazyload = true,
-      order = { "treeOffset", "buffers", "tabs", "btns" },
+      -- order = { "treeOffset", "buffers", "tabs", "btns" },
+      order = { "buffers", "tabs", "btns" },
       modules = nil,
     },
-
-    nvdash = {
-      load_on_startup = false,
-
-      header = {
-        "           ▄ ▄                   ",
-        "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
-        "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
-        "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
-        "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄  ",
-        "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
-        "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
-        "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
-        "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
-      },
-
-      buttons = {
-        { "  Find File", "Spc f f", "Telescope find_files" },
-        { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
-        { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
-        { "  Bookmarks", "Spc m a", "Telescope marks" },
-        { "  Themes", "Spc t h", "Telescope themes" },
-        { "  Mappings", "Spc c h", "NvCheatsheet" },
-      },
-    },
   },
 
-  term = {
-    winopts = { winhl = "Normal:term,WinSeparator:WinSeparator" },
-    sizes = { sp = 0.3, vsp = 0.2, ["bo sp"] = 0.3, ["bo vsp"] = 0.2 },
-    float = {
-      relative = "editor",
-      row = 0.3,
-      col = 0.25,
-      width = 0.5,
-      height = 0.4,
-      border = "single",
-    },
-  },
-
-  base46 = {
-    integrations = {},
-  },
+  cheatsheet = { theme = "grid", excluded_groups = { "terminal (t)", "autopairs", "Nvim" } }, -- simple/gridJ
 
   lsp = { signature = true },
 
-  cheatsheet = {
-    theme = "grid", -- simple/grid
-    excluded_groups = { "terminal (t)", "autopairs", "Nvim", "Opens" }, -- can add group name or with mode
+  base46 = {
+    theme_toggle = { "solarized_dark", "solarized_osaka" },
+    theme = "solarized_osaka", -- default theme
+    transparency = true, -- statusline, tabufline not transparent
+    changed_themes = {},
+    hl_add = {},
+    -- base46 integrations
+    integrations = {
+      "blankline",
+      "cmp",
+      "statusline",
+      "devicons",
+      "nvimtree",
+      "treesitter",
+      "whichkey",
+      "syntax",
+      "telescope",
+      "hop",
+      "lsp",
+      "mason",
+      "trouble",
+      "todo",
+      "dap",
+      "notify",
+    },
+    hl_override = {
+
+      -- General
+      -- LineNr = { fg = pallete.light_gray }, -- line number color
+      -- CursorLineNr = { fg = pallete.light_blue }, -- cursor line number color
+      CursorLine = { bg = "#042034" }, -- cursor line color
+      DiagnosticInfo = { fg = pallete.light_gray }, -- noice cmdline, etc.
+      DiagnosticWarn = { fg = pallete.light_gray }, -- search, warn, etc.
+      FloatBorder = { fg = pallete.light_gray }, -- border color for float windows
+      Search = { bg = pallete.light_gray, fg = pallete.dark_blue }, -- search color
+
+      -- telescope
+      TelescopePromptBorder = { fg = pallete.light_gray },
+      TelescopeBorder = { fg = pallete.light_gray },
+      TelescopeSelection = { bg = pallete.light_blue, fg = pallete.black },
+
+      -- vim mode
+      St_NormalMode = { bg = pallete.blue },
+      St_NormalModeSep = { fg = pallete.blue },
+      St_CommandMode = { bg = pallete.light_gray },
+      St_CommandModeSep = { fg = pallete.light_gray },
+      St_InsertMode = { bg = pallete.violet },
+      St_InsertModeSep = { fg = pallete.violet },
+
+      -- notify
+      NotifyINFOIcon = { fg = pallete.blue },
+      NotifyINFOTitle = { fg = pallete.blue },
+      NotifyINFOBorder = { fg = pallete.blue },
+    },
   },
 }
 
